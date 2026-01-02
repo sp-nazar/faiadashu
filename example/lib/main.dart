45b043cf9029210bc67a3222719a3312a786d5fe
Nazar <131281454+sp-nazar@users.noreply.github.com>
2026-01-02 18:37:26 +0200

    Example, StepperPage

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,47 +1,36 @@
 import 'package:faiadashu/faiadashu.dart';
-import 'package:fhir_r4/fhir_r4.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(QuestionnaireExampleApp());
+  runApp(App());
 }
 
-class QuestionnaireExampleApp extends StatelessWidget {
-  QuestionnaireExampleApp({super.key});
-
-  final _fhirResourceProvider = AssetResourceProvider.singleton(
-    questionnaireResourceUri,
-    'assets/questionnaires/phq-4.json',
-  );
-
-  final _launchContext = LaunchContext(
-    patient: Patient(
-      id: Id('example'),
-      name: [
-        HumanName(
-          text: 'Example Patient',
-        ),
-      ],
-    ),
-  );
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'PHQ-4 Questionnaire',
       localizationsDelegates: FDashLocalizations.localizationsDelegates,
       supportedLocales: FDashLocalizations.supportedLocales,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
-        useMaterial3: true,
+      home: const StepperPage(),
+    );
+  }
+}
+
+class StepperPage extends StatelessWidget {
+  const StepperPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return QuestionnaireStepperPage(
+      fhirResourceProvider: AssetResourceProvider.singleton(
+        questionnaireResourceUri,
+        'assets/questionnaires/phq-4.json',
       ),
-      home: QuestionnaireScrollerPage(
-        fhirResourceProvider: _fhirResourceProvider,
-        launchContext: _launchContext,
-        locale: const Locale('en'),
-      ),
+      launchContext: LaunchContext(),
+      locale: const Locale('en'),
     );
   }
 }
